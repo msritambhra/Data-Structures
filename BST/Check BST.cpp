5387eefab505044775d564d495ddbef61a822143
msritambhra <vritambhara@gmail.com>
2020-02-27 01:51:40 +0530

    BST

---
+++ BST/Check BST.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <queue>
+#include<climits>
+using namespace std;
+
+
+template <typename T>
+class BinaryTreeNode {
+    public : 
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+    ~BinaryTreeNode() {
+    	if(left) 
+       		delete left;
+    	if(right) 
+       		delete right;
+    }
+};
+
+
+
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    //cout << "Enter root data : ";
+    cin >> rootData;
+    if(rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while(!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+	q.pop();
+        int leftChild, rightChild;
+        //cout << "Enter left child of " << currentNode -> data << " : ";
+        cin >> leftChild;
+        if(leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode -> left =leftNode;
+            q.push(leftNode);
+        }
+        //cout << "Enter right child of " << currentNode -> data << " : ";
+        cin >> rightChild;
+        if(rightChild != -1) {
+            BinaryTreeNode<int>* rightNode = new BinaryTreeNode<int>(rightChild);
+            currentNode -> right =rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+bool isBSThelper(BinaryTreeNode<int>* root, int min, int max){
+    if(root==NULL)
+        return true;
+    
+    if(root->data<min || root->data>max)
+        return false;
+    
+    return isBSThelper(root->left,min,root->data-1) && isBSThelper(root->right,root->data+1,max);
+}
+
+bool isBST(BinaryTreeNode<int> *root){
+    
+    
+    return isBSThelper(root,INT_MIN,INT_MAX);
+
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    if(isBST(root)) {
+        cout << "true" << endl;
+    }
+    else {
+        cout << "false" << endl;
+    }
+    delete root;
+}
