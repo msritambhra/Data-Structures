968f4ad604991dfb8387b26d150567a006788209
msritambhra <vritambhara@gmail.com>
2020-02-29 13:11:17 +0530

    binary Tree

---
+++ Binary Tree/Create and Insert Duplicate Node.cpp
@@ -0,0 +1,109 @@
+/* Given a Binary Tree with N number of nodes, for each node create a new duplicate node, 
+
+and insert that duplicate as left child of the original node.
+
+*/
+
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+    public : 
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+
+    ~BinaryTreeNode() {
+    	if(left) 
+    	    delete left;
+    	if(right) 
+    	    delete right;
+    }
+};
+
+using namespace std;
+
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    //cout << "Enter root data : ";
+    cin >> rootData;
+    if(rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while(!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+	q.pop();
+        int leftChild, rightChild;
+        //cout << "Enter left child of " << currentNode -> data << " : ";
+        cin >> leftChild;
+        if(leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode -> left =leftNode;
+            q.push(leftNode);
+        }
+        //cout << "Enter right child of " << currentNode -> data << " : ";
+        cin >> rightChild;
+        if(rightChild != -1) {
+            BinaryTreeNode<int>* rightNode = new BinaryTreeNode<int>(rightChild);
+            currentNode -> right =rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+void printLevelATNewLine(BinaryTreeNode<int> *root) {
+	queue<BinaryTreeNode<int>*> q;
+	q.push(root);
+	q.push(NULL);
+	while(!q.empty()) {
+		BinaryTreeNode<int> *first = q.front();
+		q.pop();
+		if(first == NULL) {
+			if(q.empty()) {
+				break;
+			}
+			cout << endl;
+			q.push(NULL);
+			continue;
+		}
+		cout << first -> data << " ";
+		if(first -> left != NULL) {
+			q.push(first -> left);
+		}
+		if(first -> right != NULL) {
+			q.push(first -> right);
+		}
+	}
+}
+
+void insertDuplicateNode(BinaryTreeNode<int> *root) {
+    if(root==NULL)
+          return;
+    BinaryTreeNode<int>* d = new BinaryTreeNode<int>(root->data);
+   
+    d->left = root->left;
+    root->left = d;
+    insertDuplicateNode(d->left);
+    insertDuplicateNode(root->right);
+
+
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    insertDuplicateNode(root);
+    printLevelATNewLine(root);
+    delete root;
+}
