2f7c40e2b6be104d296cf6acb0f2e264e13d4c61
msritambhra <vritambhara@gmail.com>
2020-03-08 11:33:03 +0530

    Trie class updated with remove function

--- Tries & Huffman Coding/Trie.h
+++ Tries & Huffman Coding/Trie.h
@@ -54,4 +54,42 @@ class Trie {
     bool search(string word){
         return search(root,word);
     }
+
+    	void remove(TrieNode *root, string word){
+        
+        if(word.size()==0){
+            root->isTerminal = false;
+            return;
+        }
+        
+        TrieNode *child;
+        int index = word[0]-'a';
+        if(root->children[index]!=NULL)
+            child = root->children[index];
+        else
+            return; //word not found
+        
+        remove(child,word.substr(1));
+        
+        
+        //De-allocating child if its useless
+        
+        if(child->isTerminal == false){
+            
+            for(int i=0; i<26; i++){
+                
+                if(child->children[i]!=NULL)
+                    return;
+            }
+            
+            delete child;
+            root->children[index] = NULL;
+        }
+        
+        
+    }
+    
+    void remove(string word){
+        return remove(root, word);
+    }
 };
