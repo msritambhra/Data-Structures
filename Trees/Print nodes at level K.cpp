778d80847458eec6f568d436b4a3cbe0915252ea
msritambhra <vritambhara@gmail.com>
2020-03-31 21:05:05 +0530

    .

--- Trees/Print nodes at level K.cpp
+++ Trees/Print nodes at level K.cpp
@@ -14,6 +14,7 @@ void printAtLevelK(TreeNode<int>* root, int k) {
     
     if(k==0)
         cout<<root->data<<" ";
+	return;
     
     for(int i = 0; i<root->children.size(); i++){
         printAtLevelK(root->children.at(i),k-1);
