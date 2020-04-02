ca29ff262a8920e675193b269bda818f8de156f6
msritambhra <vritambhara@gmail.com>
2020-04-02 15:36:21 +0530

    .

---
+++ Binary Tree/Post-order Traversal BT.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <queue>
+#include<BinaryTreeNode.h>
+using namespace std;
+
+
+void postOrder(BinaryTreeNode<int> *root) {
+    if(root==NULL)
+      return;
+    
+  postOrder(root->left);
+  postOrder(root->right);
+    
+  cout<<root->data<<" ";
+}
+
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    //cout << "Enter root data : ";
+    cin >> rootData;
+    if(rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while(!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+	q.pop();
+        int leftChild, rightChild;
+        //cout << "Enter left child of " << currentNode -> data << " : ";
+        cin >> leftChild;
+        if(leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode -> left =leftNode;
+            q.push(leftNode);
+        }
+        //cout << "Enter right child of " << currentNode -> data << " : ";
+        cin >> rightChild;
+        if(rightChild != -1) {
+            BinaryTreeNode<int>* rightNode = new BinaryTreeNode<int>(rightChild);
+            currentNode -> right =rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+
+
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    postOrder(root);
+}
+
