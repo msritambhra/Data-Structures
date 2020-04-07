6a343c8fc9ee21c11d4e2a7be8d448672ce5e6b5
msritambhra <vritambhara@gmail.com>
2020-04-07 22:51:19 +0530

    LCA BST

---
+++ BST/LCA BST.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode {
+    public :
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+    
+    BinaryTreeNode(T data) {
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+int lcaInBST(BinaryTreeNode<int>* root , int val1 , int val2){
+    
+    if(root==NULL)
+        return -1;
+    
+    if(root->data==val1 || root->data==val2)
+        return root->data;
+    
+    int res;
+    if(val1<root->data && val2<root->data)
+         res = lcaInBST(root->left,val1,val2);
+    else if(val1>root->data && val2>root->data)
+         res = lcaInBST(root->right,val1,val2);
+    else 
+        res = root->data;
+ 
+    return res;
+
+}
+
+
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    //cout << "Enter root data : ";
+    cin >> rootData;
+    if(rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while(!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+        //cout << "Enter left child of " << currentNode -> data << " : ";
+        cin >> leftChild;
+        if(leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode -> left =leftNode;
+            q.push(leftNode);
+        }
+        //cout << "Enter right child of " << currentNode -> data << " : ";
+        cin >> rightChild;
+        if(rightChild != -1) {
+            BinaryTreeNode<int>* rightNode = new BinaryTreeNode<int>(rightChild);
+            currentNode -> right =rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    int a, b;
+    cin >> a >> b;
+    cout << lcaInBST(root, a, b);
+    delete root;
+}
